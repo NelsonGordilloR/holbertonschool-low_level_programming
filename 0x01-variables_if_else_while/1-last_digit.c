d98a287932fa0a288e2a92ebd2aba2e3dd830386
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-11 18:19:30 -0800

    1-last_digit.c fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastdigt); }
 if (lastdigt == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastdigt); }
-if (lastdigt < 5)
+if (lastdigt < 6)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigt); }
 return (0);
