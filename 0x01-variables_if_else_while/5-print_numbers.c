94cb9ae24efacdf464456a08c0bb182c98f4ecfd
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-11 19:12:26 -0800

    5-print_numbers.c added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - This function returns all single digit
+* numbers of base 10 starting from 0
+*Return: 0
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a); }
+printf("\n");
+return (0);
+}
+
