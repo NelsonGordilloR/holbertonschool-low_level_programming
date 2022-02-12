cc5e5abfdf1c29695510a64607f6fc124c44d0e0
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-11 18:49:08 -0800

    2-print_alphabet.c added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - This function returns the alphabet in lowercase
+*Return: 0
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
+
