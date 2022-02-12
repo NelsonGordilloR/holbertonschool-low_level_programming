09f0a17d13d093254938ca43c17470e60e3c5958
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-11 17:20:12 -0800

    0-positive_or_negative.c fixed with betty validation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/*This function returns a random value and indicates if it is positive, negativa or zero.*/
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n==0){
-printf("%d is zero\n", n);
-}
-if(n<0){
-printf("%d is negative\n", n);
-}
-if(n>0){
-printf("%d is positive\n", n);
-}
-return (0);
-}
+if (n == 0)
+{
+printf("%d is zero\n", n); }
+if (n < 0)
+{
+printf("%d is negative\n", n); }
+if (n > 0)
+{
+printf("%d is positive\n", n); }
+return (0); }
+
