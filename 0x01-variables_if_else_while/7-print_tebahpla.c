d42fc873558ce31db9ef27fc8ffb0abd7ec5da4f
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-14 12:25:34 -0800

    7-print_tebahpla added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - This function returns the alphabet
+ * in reverse
+ *Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 122; a >= 97; a--)
+{
+putchar(a); }
+putchar('\n');
+return (0);
+}
