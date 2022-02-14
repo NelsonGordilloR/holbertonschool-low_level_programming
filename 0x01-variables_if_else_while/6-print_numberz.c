fcd6f38c0e4d8b94e532e48fbfccb68085b64855
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-14 12:11:31 -0800

    6-print_numberz.c added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - This function returns all single digit
+* numbers of base 10 starting from 0
+*Return: 0
+*/
+int main(void)
+{
+int a;
+for (a = 48; a < 58; a++)
+{
+putchar(a); }
+putchar('\n');
+return (0);
+}
+
