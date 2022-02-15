7b8526a2187fa20c2e9b2b34973b30b65e3693fe
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-14 16:13:35 -0800

    101-print_comb4.c added

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Returns all possible different combinations
+ * of three digits, separated by comma
+ * Return: 0
+ */
+int main(void)
+{
+int a, b, c;
+for (a = 48 ; a < 56; a++)
+{
+b = a + 1;
+while (b < 57)
+{
+c = b + 1;
+while (c < 58)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if(a != 55)
+{
+putchar(44);
+putchar(32); }
+c++; }
+b++;
+}
+}
+putchar(10);
+return (0);
+}
