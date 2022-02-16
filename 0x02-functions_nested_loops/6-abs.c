a467b25af288df73d2e0c88ed04ae712f0d43335
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-16 12:10:38 -0800

    6-abs.c added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - this funcion returns
+ * the absolute value of a intiger
+ * @n: int type intiger
+ * Return: 0
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (n * -1);
+else
+return (n);
+}
