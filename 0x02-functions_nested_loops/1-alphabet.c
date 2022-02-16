37de4b21487bdc1ba9652c202927e355aa00cd0f
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-15 19:52:48 -0800

    1-alphabet.c added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - this function prints alphabet in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar(10);
+}
