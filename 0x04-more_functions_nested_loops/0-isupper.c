5b951c06a24285ceda92806be58d0149940b1a22
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-18 12:38:02 -0800

    0-isupper.c fixed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,13 @@
 #include "main.h"
+/**
+ *  _isupper - This function returns
+ * %c is 1 if is uppercase and 0 otherwise
+ *  Return: 1 if is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c < 91)
+return (1);
+else
+return (0);
+}
