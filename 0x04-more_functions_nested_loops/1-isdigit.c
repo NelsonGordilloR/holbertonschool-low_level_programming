9f3171beca694e2b2a2fa39c742fee1426297044
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-20 07:59:06 -0800

    1-isdigit.c fixed

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *  _isdigit - This function returns
- * @c integer
+ *  _isdigit - This function returns 1 if c is a digit, 0 otherwise
+ * @c: integer
  *  Return: 1 if c is digit, 0 otherwise
  */
 int _isdigit(int c)
