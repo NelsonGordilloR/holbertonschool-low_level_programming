7ecca15a3a71b9c16f6ca44e5423b13e83761f85
Nelson Gordillo <nelson.0715@hotmail.com>
2022-02-21 06:50:11 -0800

    9-fizz_buzz.c added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print number 1 to 100
+ * with additional specifications
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 1; i < 100; i++)
+{
+if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+else
+printf("%i ", i);
+}
+printf("Buzz\n");
+return (0);
+}
